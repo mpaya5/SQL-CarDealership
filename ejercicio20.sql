570d460a1ccab4264de7e4b124f88409fc0d6455
Yume Webs <71258059+yumewebs@users.noreply.github.com>
2020-10-30 10:51:14 +0100

    Delete ejercicio20.sql

--- ejercicio20.sql
+++
@@ -1,9 +0,0 @@
-/* 
-20. Seleccionar el grupo en el que trabaja el vendedor con mayor salario y
-mostrar el nombre del grupo.
- */
-
-SELECT * FROM grupos WHERE id IN
-(SELECT grupo_id FROM vendedores WHERE sueldo = (
-    SELECT MAX(sueldo) FROM vendedores
-));
