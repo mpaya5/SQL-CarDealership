4b3257f82d18e269fbbf5fb141446775b0be3cbc
Yume Webs <71258059+yumewebs@users.noreply.github.com>
2020-10-30 10:51:43 +0100

    Delete ejercicio16.sql

--- ejercicio16.sql
+++
@@ -1,6 +0,0 @@
-/* 
-16. Obtener listado de clientes atendidos por el vendedor 'David Lopez'
- */
-
-SELECT * FROM clientes WHERE vendedor_id IN 
-(SELECT id FROM vendedores WHERE nombre = 'David' AND apellidos = 'Lopez');
