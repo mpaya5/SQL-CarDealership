3a83fa87ca949b9d6fe306540e25dcf7971a3e36
Yume Webs <71258059+yumewebs@users.noreply.github.com>
2020-10-30 10:49:47 +0100

    Delete ejercicio28.sql

--- ejercicio28.sql
+++
@@ -1,8 +0,0 @@
-/* 
-28. Mostrar todos los vendedores y el numero de clientes.
-Se deben mostrar tengan o no clientes
- */
-
-SELECT v.nombre, v.apellidos, COUNT(c.id) FROM vendedores v
-LEFT JOIN clientes c ON c.vendedor_id = v.id
-GROUP BY v.id;
