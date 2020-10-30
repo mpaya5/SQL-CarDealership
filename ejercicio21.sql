f130047b1766403e2a02edda7dec1560c1afc3d1
Yume Webs <71258059+yumewebs@users.noreply.github.com>
2020-10-30 10:51:06 +0100

    Delete ejercicio21.sql

--- ejercicio21.sql
+++
@@ -1,6 +0,0 @@
-/* 
-21. Obtener los nombre y ciudades de los clientes con encargos de 3 unidades adelante
- */
-
-SELECT nombre, ciudad FROM clientes WHERE id IN
-(SELECT cliente_id FROM encargos WHERE cantidad >= 3);
